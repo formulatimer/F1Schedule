545671744e51c99be64a002bc212bc38ec985b84
formulatimer <71012769+formulatimer@users.noreply.github.com>
2024-10-08 18:16:57 +0200

    Update transform_schedule.py

--- transform_schedule.py
+++ transform_schedule.py
@@ -53,6 +53,6 @@ def transform_json(input_url, output_file):
     origin = repo.remote(name='origin')
     origin.push()
 
-input_url = 'https://raw.githubusercontent.com/theOehrly/f1schedule/refs/heads/master/schedule_2019.json'
-output_file = '2019.json'
+input_url = 'https://raw.githubusercontent.com/theOehrly/f1schedule/refs/heads/master/schedule_2020.json'
+output_file = '2020.json'
 transform_json(input_url, output_file)
